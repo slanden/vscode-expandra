d354fd66acb1249c13ede417ada327cbf08a3abf
slanden <slanden523@gmail.com>
2023-05-22 22:16:59 -0500

    build and call expandra's "core"

--- extension.js
+++ extension.js
@@ -1,4 +1,5 @@
 const vscode = require("vscode");
+const core = require("./core/expandra");
 
 /** Configures Expandra for a specific language
  * @typedef LanguageConfig
@@ -6,6 +7,18 @@ const vscode = require("vscode");
  * the generated code
  */
 
+/** UTF-8 encoder */
+const UTF8 = (() => {
+  let dec = new TextDecoder();
+  let enc = new TextEncoder();
+  return {
+    // Must bind; probably becuase the methods' `this`
+    // is literally this, but they expect the `this`
+    // of their respective objects
+    decode: dec.decode.bind(dec),
+    encode: enc.encode.bind(enc),
+  };
+})();
 
 /**
  * @param {vscode.ExtensionContext} context
@@ -31,16 +44,20 @@ function deactivate() {}
 /** Replace the string at the cursor with a snippet
  * of the expanded version of the string, if it
  * follows the proper syntax.
+ * @param {LanguageConfig} lang
+ * @param {vscode.Range} range
  */
-function expand() {
+function expand(lang, range) {
   let editor = vscode.window.activeTextEditor;
   if (!editor) return;
 
+  let res = UTF8.decode(core.wasm_expand(
+    UTF8.encode(editor.document.getText(range)),
+    UTF8.encode(lang.template),
+  ));
   editor.insertSnippet(
-    new vscode.SnippetString(`Put snippet here $1`),
-    editor.document.getWordRangeAtPosition(
-      editor.selection.active,
-    ),
+    new vscode.SnippetString(res /* `Put snippet here $1` */),
+    range,
   );
 }
 
@@ -76,7 +93,10 @@ function provideCompletionItems(document, position) {
   );
   completion.documentation = "Converts the string to source code";
   // Trigger a command to lazily calculate the result
-  completion.command = { command: "expandra.expand" };
+  completion.command = {
+    command: "expandra.expand",
+    arguments: [lang, completion.range],
+  };
   // Setting `true` fixes the "UI refresh flash"
   return new vscode.CompletionList([completion], true);
 }
